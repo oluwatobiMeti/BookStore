022ee8f85b10b17d097c3e75c956d438b345fc48
Oluwatobi metibemu <abismetibemu@gmail.com>
2024-02-18 15:11:21 +0100

    init commit

---
+++ book_store/lib/utils/app-constant.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class AppConstant {
+  static String appMainName = 'Ecomm';
+  static String appPoweredBy = 'powered by oluwatobi metibemu';
+  static const appMainColor = Color(0xFFbf1b08);
+  static const appSecondaryColor = Color(0xFF981206);
+  static const appTextColor = Color(0xFFFBF5F4);
+  static const appStatusBarColor = Color(0xFFFBF5F4);
+}
